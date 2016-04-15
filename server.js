5a92a5b47b17e8d29f82dba44ca9639c5cf9890e
Israel Kollie <zikollie@gmail.com>
2016-04-15 14:33:15 +0100

    Initialize repo

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+//module.exports = middleware;
+//var middleware = require('./middleware.js');
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('Private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+// app.get('/', function (req, res) {
+// 	res.send('Hello Express');
+// });
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res){
+	res.send('About Us');
+});
+
+
+app.use(express.static(__dirname + '/public'))
+
+app.listen(PORT, function () {
+	console.log('Express server started on port ' + PORT + '!');
+});
+
